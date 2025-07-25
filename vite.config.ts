ff40a5f85022c4a620b422d65d7c0dbf1a7f0e1e
chdavid-in <david.official.ux.com>
2025-07-25 08:28:00 +0530

    my deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio_2025/", // 👈 Add this line to match your repo name
   server: {
     host: "::",
     port: 8080,
